9110b8ee2e4e82eb5d6c1c001a5d8eb480a8331f
Any97Cris <criscianysilva1997@gmail.com>
2022-12-29 15:48:23 -0400

    DAO

--- DAO/ProdutoDAO.php
+++ DAO/ProdutoDAO.php
@@ -3,5 +3,22 @@ require_once 'model/Produto.php';
 
 class ProdutoDAU implements produtoDAO{
 
+    private $pdo;
+
+    public function __construct(PDO $driver){
+        $this->pdo = $driver;
+    }
+
+    public function add(Produto $pro){
+        $sql = $this->pdo->prepare("INSERT INTO produto(id, nome, preco, quantidade) VALUES (default,:nome,:preco,:quantidade)");
+        $sql->bindValue(':nome', $pro->getNome());
+        $sql->bindValue('preco', $pro->getPreco());
+        $sql->bindValue('quantidade', $pro->getQuantidade());
+        $sql->execute();
+
+        $pro->setId($this->pdo->lastInsertId());
+        return $pro;
+    }
+
 }
 ?>
