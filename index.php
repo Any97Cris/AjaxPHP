6f83be9874e5e124e2011fa34ab6d1bc55068ae2
Any97Cris <criscianysilva1997@gmail.com>
2022-12-29 09:09:44 -0400

    modals cadastrar,editar e deletar

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 </head>
 <body class="bg-dark">
     <div class="container">
-        <h1 class="mt-5 mb-5 text-center">Produtos Informática</h1>
+        <h1 class="mt-5 mb-5 text-center colorTextWhite">Produtos Informática</h1>
         <div class="input-group text-center mb-5">
-            <button class="btn btn-warning">Cadastrar Produto </button>
+            <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">Cadastrar Produto </button>
         </div>
             
 
@@ -36,13 +36,103 @@
                     <td>Teste</td>
                     <td>Teste</td>
                     <td class="text-center">
-                        <a href="#" class="btns"><ion-icon name="create-outline"></ion-icon></a>
-                        <a href="#" class="btns"><ion-icon name="trash-outline"></a>
+                        <a class="btns" data-bs-toggle="modal" data-bs-target="#exampleModal2"><ion-icon name="create-outline"></ion-icon></a>
+                        <a class="btns" data-bs-toggle="modal" data-bs-target="#exampleModal3"><ion-icon name="trash-outline"></a>
                     </td>
                 </tr>                
             </tbody>
         </table>
     </div>
+
+    <!--MODAL CADASTRAR-->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Cadatrar Produto</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+                <form id="formCadastrar">
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Nome Produto</label>
+                            <input type="text" class="form-control" name="produto" id="produtoId" placeholder="Digite o nome do Produto...">
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Preço</label>
+                            <input type="text" class="form-control" name="preco" id="precoId" placeholder="R$ 00,00">
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Quantidade</label>
+                            <input type="text" class="form-control" name="quantidade" id="quantidadeId" placeholder="Quantidade">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                        <input type="submit" class="btn btn-primary" value="Salvar"/>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!--FIM MODAL CADASTRAR-->
+
+    <!--MODAL EDITAR-->
+    <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Editar Produto</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form id="formEditar">
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Nome Produto</label>
+                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Digite o nome do Produto...">
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Preço</label>
+                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="R$ 00,00">
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Quantidade</label>
+                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Quantidade">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                        <button type="button" class="btn btn-primary">Salvar</button>
+                    </div>
+                </form>    
+            </div>
+        </div>
+    </div>
+    <!--FIM MODAL EDITAR-->
+
+    <!--MODAL DELETAR-->
+    <div class="modal fade" id="exampleModal3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Deletar Produto</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Deseja realmente deletar?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                <button type="button" class="btn btn-danger">Sim</button>
+            </div>
+            </div>
+        </div>
+    </div>
+    <!--FIM MODAL DELETAR-->
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
