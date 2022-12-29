4c405bfc64c0694761cc2afae5cdb9028c98c121
Any97Cris <criscianysilva1997@gmail.com>
2022-12-29 10:42:43 -0400

    gets e sets

---
+++ Model/Produto.php
@@ -0,0 +1,48 @@
+<?php
+
+class produto
+{
+    private $id;
+    private $nome;
+    private $preco;
+    private $quantidade;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($idd){
+        $this->id = trim($idd);
+    }
+
+    public function getNome(){
+        return $this->$nome;
+    }
+
+    public function setNome($n){
+        $this->$nome = ucwords(trim($n));
+    }
+
+    public function getPreco(){
+        return $this->$preco;
+    }
+
+    public function setPreco($p){
+        $this->$preco = $p;
+    }
+
+    public function getQuantidade(){
+        return $this->$quantidade;
+    }
+
+    public function setQuantidade($q){
+        $this->$quantidade = $q;
+    }
+}
+
+interface produtoDAO{
+    public function add(Produto $pro);
+}
+
+?>
